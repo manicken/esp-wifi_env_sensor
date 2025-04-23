713cae313b37dadbdb91dabe69b54e5b677291fd
manicken <65423412+manicken@users.noreply.github.com>
2025-04-23 23:13:06 +0200

    begin write on REGO600 code

--- src/REGO600.cpp
+++ src/REGO600.cpp
@@ -1,6 +1,26 @@
 
 #include "REGO600.h"
 
+const REGO600::Request RequestsWholeLCD[] = {
+    {0x0000, 0x0000},
+    {0x0001, 0x0001},
+    {0x0002, 0x0002},
+    {0x0003, 0x0003}
+};
+const size_t RequestsWholeLCD_Count = 4;
+const REGO600::Request RequestsAllTemperatures[] = {
+    {0x0209, 0x0000},
+    {0x020A, 0x0000},
+    {0x020B, 0x0000},
+    {0x020D, 0x0000},
+    {0x020E, 0x0000},
+    {0x020F, 0x0000},
+    {0x0210, 0x0000},
+    {0x0211, 0x0000},
+    {0x0212, 0x0000},
+};
+const size_t RequestsAllTemperatures_Count = 9;
+
 REGO600::REGO600()
     : server(REGO600_WS_PORT), ws("/rego600ws"), UART2(Serial2) // Init UART2 as HardwareSerial(2)
 {}
@@ -11,12 +31,14 @@ void REGO600::onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
         AwsFrameInfo *info = (AwsFrameInfo*)arg;
 
         if (info->opcode == WS_BINARY) {
+            lastCommand = Command::None;
             UART2.write(data, len);
         }
     }
 }
 
 void REGO600::setup() {
+    uartTxBuffer[0] = 0x81; // allways 0x81, never to be changed
     UART2.begin(19200, SERIAL_8N1, 16, 17);  // Set correct RX/TX pins for UART2
     ws.onEvent([this](AsyncWebSocket *s, AsyncWebSocketClient *c, AwsEventType t, void *a, uint8_t *d, size_t l) {
         this->onWsEvent(s, c, t, a, d, l);
@@ -24,20 +46,65 @@ void REGO600::setup() {
 
     server.addHandler(&ws);
     server.begin();
+
+}
+
+void REGO600::BeginRetreiveWholeLCD() {
+    lastCommand = Command::ReadLcd;
+    currentExpectedRxLength = (uint8_t)CommandRxDataLenght::ReadLcd;
+    requestCount = RequestsWholeLCD_Count;
+    requestIndex = 0;
+    requests = RequestsWholeLCD;
+    uartTxBuffer[1] = (uint8_t)lastCommand;
+    SetRequest(requests[requestIndex++]);
+    CalcAndSetTxChecksum();
+    UART2.write(uartTxBuffer, REGO600_UART_TX_BUFFER_SIZE);
+}
+
+void REGO600::SetRequest(REGO600::Request req) {
+    uartTxBuffer[2] = (req.address >> 14) & 0x7F;
+    uartTxBuffer[3] = (req.address >> 7) & 0x7F;
+    uartTxBuffer[4] = req.address & 0x7F;
+    uartTxBuffer[5] = (req.data >> 14) & 0x7F;
+    uartTxBuffer[6] = (req.data >> 7) & 0x7F;
+    uartTxBuffer[7] = req.data & 0x7F;
+}
+
+void REGO600::CalcAndSetTxChecksum() {
+    uint8_t chksum = 0;
+    for (int i=REGO600_UART_TX_CHKSUM_START_INDEX;i<REGO600_UART_TX_CHKSUM_END_INDEX;i++) {
+        chksum ^= uartTxBuffer[i];
+    }
+    uartTxBuffer[REGO600_UART_TX_CHKSUM_END_INDEX] = chksum;
 }
 
 void REGO600::task_loop() {
-    // Read all available UART data
-    while (UART2.available()) {
-        if (uartBufferIndex < REGO600_UART_BUFFER_SIZE) {
-            uartBuffer[uartBufferIndex++] = UART2.read();
-            lastByteTime = millis(); // update last byte time
+    if (lastCommand == Command::None) {
+        // Read all available UART data
+        while (UART2.available()) {
+            if (uartBufferIndex < REGO600_UART_RX_BUFFER_SIZE) {
+                uartRxBuffer[uartBufferIndex++] = UART2.read();
+                lastByteTime = millis(); // update last byte time
+            }
+        }
+
+        // If buffer is not empty and timeout passed, send it
+        if (uartBufferIndex > 0 && (millis() - lastByteTime > FLUSH_TIMEOUT_MS)) {
+            ws.binaryAll(uartRxBuffer, uartBufferIndex);
+            uartBufferIndex = 0;
         }
     }
+    else {
+        while (UART2.available()) {
+            if (uartBufferIndex < REGO600_UART_RX_BUFFER_SIZE) {
+                uartRxBuffer[uartBufferIndex++] = UART2.read();
+                if (uartBufferIndex == currentExpectedRxLength) {
+                    // RX is done
+                    if (lastCommand == Command::ReadLcd) {
 
-    // If buffer is not empty and timeout passed, send it
-    if (uartBufferIndex > 0 && (millis() - lastByteTime > FLUSH_TIMEOUT_MS)) {
-        ws.binaryAll(uartBuffer, uartBufferIndex);
-        uartBufferIndex = 0;
+                    }
+                }
+            }
+        }
     }
 }
--- src/REGO600.h
+++ src/REGO600.h
@@ -4,7 +4,10 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 
-#define REGO600_UART_BUFFER_SIZE 256
+#define REGO600_UART_TX_CHKSUM_START_INDEX 2
+#define REGO600_UART_TX_BUFFER_SIZE 9 // allways 9 in size
+#define REGO600_UART_TX_CHKSUM_END_INDEX (REGO600_UART_TX_BUFFER_SIZE-1)
+#define REGO600_UART_RX_BUFFER_SIZE 42 // 42 is the max rx size from REGO600
 #define REGO600_WS_PORT 8080
 
 
@@ -13,8 +16,9 @@ public:
     REGO600();
     void setup();
     void task_loop();
+    void BeginRetreiveWholeLCD();
 
-    enum Command {
+    enum class Command : int16_t{
         None = -1,
         ReadPanel = 0x00,
         WritePanel = 0x01,
@@ -23,20 +27,45 @@ public:
         ReadLastError = 0x40,
         ReadPrevError = 0x44
     };
+    enum class CommandRxDataLenght : uint8_t {
+        ReadPanel = 5,
+        WritePanel = 1,
+        ReadRegister = 5,
+        ReadLcd = 42,
+        ReadLastError = 42,
+        ReadPrevError = 42
+    };
+    struct Request {
+        uint16_t address;
+        uint16_t data;
+    };
 private:
     
-    uint8_t uartBuffer[REGO600_UART_BUFFER_SIZE];
+    uint8_t uartTxBuffer[REGO600_UART_TX_BUFFER_SIZE]; 
+    uint8_t uartRxBuffer[REGO600_UART_RX_BUFFER_SIZE];
     size_t uartBufferIndex = 0;
     unsigned long lastByteTime = 0;
     const unsigned long FLUSH_TIMEOUT_MS = 50; // flush after 50ms idle
 
     Command lastCommand = Command::None;
     uint8_t currentExpectedRxLength = 0;
-    uint8_t currentRxCount = 0;
+    //size_t currentRxCount = 0;
+    const Request *requests; // contains a list of reg adresses to read
+    uint8_t requestIndex = 0;
+    uint8_t requestCount = 0;
+
+    char lcdData[20*4];
+    uint16_t temperatures[9];
 
     AsyncWebServer server;
     AsyncWebSocket ws;
     HardwareSerial& UART2;
     void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+    void SetRequest(REGO600::Request req);
+    void CalcAndSetTxChecksum();
 };
+const REGO600::Request RequestsWholeLCD[];
+const REGO600::Request RequestsAllTemperatures[];
 
+extern const size_t RequestsWholeLCD_Count;
+extern const size_t RequestsAllTemperatures_Count;
