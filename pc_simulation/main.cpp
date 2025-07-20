a84ad2c2381e6b7900a5942196b361b92927bb11
Jannik Svensson <tde04014@gmail.com>
2025-07-20 13:30:47 +0200

    add CountBlockItems

--- pc_simulation/main.cpp
+++ pc_simulation/main.cpp
@@ -20,6 +20,9 @@
         std::cout << "Warning (line " << line << ", column " << column << "): " << msg << std::endl;
     }
 
+    inline bool StrEqualsIC(const char* strA, const char* strB) { return CharArray::equalsIgnoreCase(strA, strB); }
+    
+
     int CountTokens(char* buffer) {
         int count = 0;
         char* p = buffer;
@@ -39,11 +42,16 @@
         }
         return count;
     }
+
     struct Token {
         const char* text;
         int line;
         int column;
+        int itemsInBlock;
     };
+    inline bool IsType(const Token& t, const char* str) { return StrEqualsIC(t.text, str); }
+    
+
     bool Tokenize(char* buffer, Token* tokens, int tokenCount) {
         int line = 1;
         int column = 1;
@@ -160,53 +168,69 @@
         int tokenIndex;
     };
     int Count_IfTokens(Token* tokens, int tokenCount) {
-        int count = 0;
+        int currLevel = 0;
+        int maxLevel = 0;
         for (int i = 0; i < tokenCount; i++) {
-            if (CharArray::equalsIgnoreCase(tokens[i].text, "if"))
-                count++;
+            if (IsType(tokens[i], "if")) {
+                currLevel++;
+                if (currLevel > maxLevel) maxLevel = currLevel;
+            }
+            else if (IsType(tokens[i], "endif")) {
+                currLevel--;
+            }
         }
-        return count;
+        return maxLevel;
     }
     bool VerifyBlocks(Token* tokens, int tokenCount) {
         int onLevel = 0;
         int ifLevel = 0;
         int ifTokenCount = Count_IfTokens(tokens, tokenCount);
+        if (ifTokenCount <= 0) ifTokenCount = 1;
         Token* ifStack = new Token[ifTokenCount];
         int ifStackIndex = 0;
         Token lastOn = {nullptr, 0, 0};
         bool otherErrors = false;
 
         bool expecting_do_then = false;
+        int lastControlIndex = 0;
 
         for (int i = 0; i < tokenCount; i++) {
-            if (CharArray::equalsIgnoreCase(tokens[i].text, "if")) {
+            Token& token = tokens[i];
+            if (IsType(token, "if")) {
                 ifLevel++;
-                ifStack[ifStackIndex++] = tokens[i];
+                ifStack[ifStackIndex++] = token;
                 expecting_do_then = true;
+                lastControlIndex = i;
             }
-            else if (CharArray::equalsIgnoreCase(tokens[i].text, "elseif")) {
+            else if (IsType(token, "elseif")) {
                 expecting_do_then = true;
+                lastControlIndex = i;
             }
-            else if (CharArray::equalsIgnoreCase(tokens[i].text, "on")) {
+            else if (IsType(token, "on")) {
                 if (ifLevel != 0 || onLevel != 0) {
-                    ReportError("'on' block cannot be nested", tokens[i].line, tokens[i].column);
+                    ReportError("'on' block cannot be nested", token.line, token.column);
                     otherErrors = true;
                 } else {
-                    lastOn = tokens[i];
+                    lastOn = token;
                     onLevel++;
                     expecting_do_then = true;
+                    lastControlIndex = i;
                 }
             }
-            else if (CharArray::equalsIgnoreCase(tokens[i].text, "do") || CharArray::equalsIgnoreCase(tokens[i].text, "then")) {
+            else if (IsType(token, "do") || IsType(token, "then")) {
                 if (!expecting_do_then) {
-                    ReportError("'do'/'then' without preceding 'if' or 'on'", tokens[i].line, tokens[i].column);
+                    ReportError("'do'/'then' without preceding 'if' or 'on'", token.line, token.column);
+                    otherErrors = true;
+                }
+                else if (lastControlIndex + 1 == i) {
+                    ReportError("Missing condition between 'if/on/elseif' and 'then/do'", token.line, token.column);
                     otherErrors = true;
                 }
                 expecting_do_then = false;
             }
-            else if (CharArray::equalsIgnoreCase(tokens[i].text, "endif")) {
+            else if (IsType(token, "endif")) {
                 if (ifLevel == 0) {
-                    ReportError("'endif' without matching 'if'", tokens[i].line, tokens[i].column);
+                    ReportError("'endif' without matching 'if'", token.line, token.column);
                     otherErrors = true;
                 }
                 else {
@@ -216,23 +240,23 @@
                 }
 
                 if (expecting_do_then) {
-                    ReportError("missing 'do' after last 'if'", tokens[i].line, tokens[i].column);
+                    ReportError("missing 'do' after last 'if'", token.line, token.column);
                     otherErrors = true;
                 }
             }
-            else if (CharArray::equalsIgnoreCase(tokens[i].text, "endon")) {
+            else if (IsType(token, "endon")) {
                 if (onLevel == 0) {
-                    ReportError("'endon' without matching 'on'", tokens[i].line, tokens[i].column);
+                    ReportError("'endon' without matching 'on'", token.line, token.column);
                     otherErrors = true;
                 } else
                     onLevel--;
 
                 if (expecting_do_then) {
-                    ReportError("missing 'do' after last 'on'", tokens[i].line, tokens[i].column);
+                    ReportError("missing 'do' after last 'on'", token.line, token.column);
                     otherErrors = true;
                 }
             } else if (onLevel == 0 && ifLevel == 0) {
-                ReportError("tokens cannot be outside root blocks", tokens[i].line, tokens[i].column);
+                ReportError("tokens cannot be outside root blocks", token.line, token.column);
                 otherErrors = true;
             }
         }
@@ -258,20 +282,21 @@
     // then all contents between if and do/then could be easly merged to one line
     void MergeConditions(Token* tokens, int& tokenCount) {
         for (int i = 0; i < tokenCount; i++) {
-            if (CharArray::equalsIgnoreCase(tokens[i].text, "if") == false) continue;
+            if ((IsType(tokens[i], "if") || IsType(tokens[i], "elseif")) == false) continue;
 
             int start = i + 1;
             int end = -1;
             
             // Find "do" or "then"
             for (int j = start; j < tokenCount; ++j) {
-                if (CharArray::equalsIgnoreCase(tokens[j].text, "do") || CharArray::equalsIgnoreCase(tokens[j].text, "then")) {
+                if (IsType(tokens[j], "do") || IsType(tokens[j], "then")) {
                     end = j;
                     break;
                 }
             }
-            if (start == end) {
-                ReportWarning("MergeConditions - empty if condition", tokens[i].line, tokens[i].column);
+            // as this is actually taken care of in VerifyBlocks this check don't really need to be here
+            if (start == end) { 
+                ReportError("MergeConditions - empty if condition", tokens[i].line, tokens[i].column);
                 continue;
             }
 
@@ -286,11 +311,12 @@
 
 
             for (int j = start+1; j < end; ++j) {
-                const char* t = tokens[j].text;
-                if (CharArray::equalsIgnoreCase(t, "AND")) t = "&&";
-                else if (CharArray::equalsIgnoreCase(t, "OR")) t = "||";
+                const Token& token = tokens[j];
+                const char* tText = token.text;
+                if (IsType(token, "AND")) tText = "&&";
+                else if (IsType(token, "OR")) tText = "||";
 
-                strcat(writePtr, t);
+                strcat(writePtr, tText);
             }
 
             // Remove tokens between start+1 and end-1
@@ -308,44 +334,48 @@
     void MergeActions(Token* tokens, int& tokenCount) {
         for (int i = 0; i < tokenCount; i++) {
             const char* text = tokens[i].text;
-            if (!(CharArray::equalsIgnoreCase(text, "then") ||
-                 CharArray::equalsIgnoreCase(text, "do") ||
-                 CharArray::equalsIgnoreCase(text, "and") ||
-                 CharArray::equalsIgnoreCase(text, "else") ||
-                 CharArray::equalsIgnoreCase(text, "endif"))) {
+            if (!(StrEqualsIC(text, "then") ||
+                 StrEqualsIC(text, "do") ||
+                 StrEqualsIC(text, "and") ||
+                 StrEqualsIC(text, "else") ||
+                 StrEqualsIC(text, "endif"))) {
                     continue;
             } 
             int start = i + 1;
             int end = -1;
-            ReportInfo("MergeActions found start:", tokens[i].line, tokens[i].column);
+            //ReportInfo("MergeActions found start:", tokens[i].line, tokens[i].column);
             
             for (int j = start; j < tokenCount; ++j) {
                 const char* text = tokens[j].text;
                 if (tokens[j].line == 23) { std::cout << "@ line 23: " << std::string(tokens[j].text) << std::endl;
                     //return;
                 }
-                if (!(CharArray::equalsIgnoreCase(text, "and") ||
-                      CharArray::equalsIgnoreCase(text, "if") ||
-                      CharArray::equalsIgnoreCase(text, "else") ||
-                      CharArray::equalsIgnoreCase(text, "elseif") ||
-                      CharArray::equalsIgnoreCase(text, "endon") ||
-                      CharArray::equalsIgnoreCase(text, "endif"))) continue;
+                if (!(StrEqualsIC(text, "and") ||
+                      StrEqualsIC(text, "if") ||
+                      StrEqualsIC(text, "else") ||
+                      StrEqualsIC(text, "elseif") ||
+                      StrEqualsIC(text, "endon") ||
+                      StrEqualsIC(text, "endif"))) continue;
 
                 end = j;
                 break;
             }
             if (start == end) {
-                //  this is not even a warning
-                ReportInfo("MergeActions - empty action", tokens[i].line, tokens[i].column);
+                // as currently this also checks between 
+                // different endif endif and similar situations where actions is not necessary
+                // this should not currently be a warning
+                // can be taken care of in later checks or at construct time where other checks
+                // need to be taken care of beforehand
+                //ReportWarning("MergeActions - empty action", tokens[i].line, tokens[i].column);
                 continue;
             }
 
             if (end == -1) {
-                ReportError("MergeActions - malformed action block", tokens[i].line, tokens[i].column);
+                //ReportError("MergeActions - malformed action block", tokens[i].line, tokens[i].column);
                 continue;
             }
 
-            ReportInfo("MergeActions found end:", tokens[end].line, tokens[end].column);
+            //ReportInfo("MergeActions found end:", tokens[end].line, tokens[end].column);
 
             // Merge tokens from [start, end)
             char* writePtr = (char*)tokens[start].text;
@@ -366,16 +396,71 @@
         int writeIdx = 0;
         for (int readIdx = 0; readIdx < tokenCount; ++readIdx) {
             if (tokens[readIdx].line == -1) continue;
+            const char* tokenText = tokens[readIdx].text;
             // one line action tokens
-            if (CharArray::equalsIgnoreCase(tokens[readIdx].text, "and")) continue;
+            if (StrEqualsIC(tokenText, "and")) continue;
             // then and do are no longer nessesary
-            if (CharArray::equalsIgnoreCase(tokens[readIdx].text, "then")) continue;
-            if (CharArray::equalsIgnoreCase(tokens[readIdx].text, "do")) continue;
+            //if (StrEqualsIC(tokenText, "then")) continue;
+            //if (StrEqualsIC(tokenText, "do")) continue;
             /*if (writeIdx != readIdx)*/ tokens[writeIdx] = tokens[readIdx];
             ++writeIdx;
         }
         tokenCount = writeIdx;
     }
+
+    void CountBlockItems(Token* tokens, int tokenCount) {
+        for (int i = 0; i < tokenCount; ++i) {
+            Token& tok = tokens[i];
+
+            if (IsType(tok, "on")) {
+                int count = 0;
+                for (int j = i + 1; j < tokenCount; ++j) {
+                    Token& innerTok = tokens[j];
+                    if (IsType(innerTok, "endon")) {
+                        break;
+                    }
+                    count++;
+                }
+                tok.itemsInBlock = count;
+            }
+            else if (IsType(tok, "if")) {
+                int count = 1;
+                int level = 1;
+                for (int j = i + 1; j < tokenCount; ++j) {
+                    Token& innerTok = tokens[j];
+                    if (IsType(innerTok, "if")) level++;
+                    if (IsType(innerTok, "endif")) {
+                        level--;
+                        if (level == 0) break;
+                    }
+                    if (level == 1 && (IsType(innerTok, "elseif") || IsType(innerTok, "else"))) {
+                        count++;
+                    }
+                }
+                tok.itemsInBlock = count;
+            }
+            else if (IsType(tok, "then") || IsType(tok, "else")) {//} || IsType(tok, "elseif")) {
+                int count = 0;
+                int level = 1;
+                for (int j = i + 1; j < tokenCount; ++j) {
+                    Token& innerTok = tokens[j];
+                    if (IsType(innerTok, "if")) level++;
+                    if (IsType(innerTok, "endif")) {
+                        level--;
+                        if (level == 0) break;
+                    }
+                    if (level == 1 && (IsType(innerTok, "elseif") || IsType(innerTok, "else"))) {
+                        break;
+                    }
+                    if (level == 1) count++;
+                }
+                tok.itemsInBlock = count;
+            }
+        }
+    }
+
+
+    
 
     int main() {
         std::cout << "Running on Windows (MinGW)" << std::endl;
@@ -406,13 +491,15 @@
                 std::cout << std::endl << "Mergin Actions:" << std::endl;
                 MergeActions(tokens, tokenCount);
                 std::cout << "[OK]" << std::endl;
-                std::cout << std::endl;
+                std::cout << std::endl << "CountBlockItems" << std::endl;
+
+                CountBlockItems(tokens, tokenCount);
                 // debug print
                 //int count = 0;
                 for (int i=0;i<tokenCount;i++) {
                     //if (tokens[i].line == -1) continue;
                     //count++;
-                    std::cout << "Token("<<i<<"): " << "(line:" << std::to_string(tokens[i].line) << ", col:" << std::to_string(tokens[i].column) << ")\t" << tokens[i].text << std::endl;
+                    std::cout << "Token(" << i << "): " << "(line:" << std::to_string(tokens[i].line) << ", col:" << std::to_string(tokens[i].column) << ", itemCount:" << tokens[i].itemsInBlock << ")\t" << tokens[i].text << std::endl;
                 }
                 //std::cout << "Token count after action merge: " << count << std::endl;
             }
